9473b3e71ba8a2b96425b9faddfa4436354349cd
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-04-07 15:10:31 +0900

    タイトルを修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'paper-downloader-nuxt2',
+    title: 'Paper Downloader',
     htmlAttrs: {
       lang: 'en'
     },
