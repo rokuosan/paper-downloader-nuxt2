a8f6c9799229b90642d4609339f85991c5bed176
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-04-07 15:00:39 +0900

    Footer を追加

---
+++ components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-screen gradient fixed bottom-0">
+    <nav class="mx-4 md:content-center grid md:grid-cols-2 md:h-20 h-24">
+      <div class="text-base justify-self-start self-center">
+        Copyright Ⓒ 2022 rokuosan , All rights reserved.
+      </div>
+      <div class="md:justify-self-end self-center text-base underline flex gap-4">
+        <a href="https://papermc.io/api/docs" target="_blank" rel="noopener noreferrer">Paper Downloads API</a>
+        <a href="https://github.com/rokuosan/paper-downloader-nuxt2/" target="_blank" rel="noopener noreferrer">GitHub</a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+</style>
